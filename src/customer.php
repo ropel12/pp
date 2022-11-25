81857065411c5930d52f78330b18f053ebe1c7e0
ropel12 <poliwaras32@gmail.com>
2022-11-25 13:45:33 +0700

    update v3.0.0

--- src/customer.php
+++ src/customer.php
@@ -5,12 +5,12 @@ namespace Satrio\Library;
 
 class Customer
 {
-    public function __construct(private string $data)
+    public function __construct(private string $data = "guest")
     {
     }
 
-    public function Hello(): string
+    public function Hello(string $data): string
     {
-        return "data: $this->data";
+        return "data: $this->data , $data";
     }
 }
